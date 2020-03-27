e624b4289ccd3974aef52c380d58bd95bcf74f94
abdelazizElemary <abdelaziz528@gmail.com>
2020-03-27 19:10:42 +0200

    addded degree celcius symbol

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(express.static(publicDirectory))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather App',
+        title: 'Weather Forecast',
         name: 'Abdelaziz Elemary'
     })
 })
