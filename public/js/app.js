3871aa7ea3698847d08c660820221b9c2f2bdac9
abdelazizElemary <abdelaziz528@gmail.com>
2020-03-29 01:27:09 +0200

    adding autocomplete feature to the search

--- public/js/app.js
+++ public/js/app.js
@@ -35,3 +35,24 @@ weatherForm.addEventListener('submit', (e) => {
     })
 
 })
+
+
+jQuery(function () {
+    jQuery("#f_elem_city").autocomplete({
+        source: function (request, response) {
+            jQuery.getJSON(
+                "https://secure.geobytes.com/AutoCompleteCity?key=7c756203dbb38590a66e01a5a3e1ad96&callback=?&q=" + request.term,
+                function (data) {
+                    response(data);
+                }
+            );
+        },
+        minLength: 3,
+        select: function (event, ui) {
+            var selectedObj = ui.item;
+            jQuery("#f_elem_city").val(selectedObj.value);
+            return false;
+        }
+    });
+    jQuery("#f_elem_city").autocomplete("option", "delay", 100);
+});
